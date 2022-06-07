8af8950acc8227ad27f364645f3616c126ddf8a1
jerumanu <70507937+jerumanu@users.noreply.github.com>
2022-06-08 00:02:44 +0300

    Remove index from urls

--- post/urls.py
+++ post/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path('', views.index, name='index'),
     path('upload/', views.upload, name = 'upload-post'),
     path('update/<int:post_id>', views.update_post),
     path('delete/<int:post_id>', views.delete_post),
