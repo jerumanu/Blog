49fdba7263edd2a30e064daed54bc7746cb89b15
jerumanu <emmanueljeru200@gmail.com>
2022-06-07 22:51:26 +0300

    add authetications urls

--- post/urls.py
+++ post/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+
 urlpatterns = [
-    path('', views.index, name = 'index'),
+    path('index', views.index, name='index'),
     path('upload/', views.upload, name = 'upload-post'),
     path('update/<int:post_id>', views.update_post),
-    path('delete/<int:post_id>', views.delete_post)
+    path('delete/<int:post_id>', views.delete_post),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
