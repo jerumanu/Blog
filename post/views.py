8fe949ee8a0ff04b65e513eacd6ecc3589a19b76
jerumanu <emmanueljeru200@gmail.com>
2022-06-07 22:51:58 +0300

    add authetications in views

--- post/views.py
+++ post/views.py
@@ -3,11 +3,33 @@ from .models import *
 from .forms import *
 from django.http import HttpResponse
 
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+ 
+# Views
 
 
 def index(request):
     post = Post.objects.all()
     return render(request, 'post/blog.html', {'post': post})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username = username, password = password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+
+
 
 
 def upload(request):
