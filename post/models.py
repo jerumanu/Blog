0749f07d38c4a023c2febbd415b7bb54c0045ebc
jerumanu <emmanueljeru200@gmail.com>
2022-06-07 12:09:05 +0300

    create class

--- post/models.py
+++ post/models.py
@@ -1,2 +1,18 @@
 from django.db import models
 
+
+
+class Post(models.Model):
+    title=models.CharField(max_length=50)
+    blog =models.CharField(max_length=400 )
+    # author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now= True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    name= models.CharField(max_length=50)   
+
+
+
+   
+
+
