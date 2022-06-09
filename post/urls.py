5808e93916f2add897cad5942f0b51a06366aadf
jerumanu <emmanueljeru200@gmail.com>
2022-06-09 21:55:33 +0300

    mande changes to urls in nav

--- post/urls.py
+++ post/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('home/', views.mypost, name='home'),
+    path('index', views.index, name='index'),
+    path('', views.mypost, name='home'),
     
     path('upload/', views.upload, name = 'upload-post'),
     path('update/<int:post_id>', views.update_post  ),
