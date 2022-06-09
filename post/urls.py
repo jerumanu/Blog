39b3b24dc42c691d11c98f3aa4589c1c8dd9fb0c
jerumanu <emmanueljeru200@gmail.com>
2022-06-09 10:29:50 +0300

    made changes to disign

--- post/urls.py
+++ post/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('update/<int:post_id>', views.update_post  ),
     path('delete/<int:post_id>', views.delete_post),
     path('login/', auth_views.LoginView.as_view(), name='login'),
+    
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
 ]
